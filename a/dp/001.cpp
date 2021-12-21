7b1a6dc34798e64bfeb29b1798e6ae6aed4610a0
NF <noah.ftnx@gmail.com>
2021-12-21 11:03:00 +0200

    m

--- a/dp/001.cpp
+++ a/dp/001.cpp
@@ -1,7 +1,5 @@
-#include <vector>
-using namespace std;
 
-// uint64_t count_bf(int n);
+uint64_t count_bf(int n);
 // uint64_t count_td(int n);
 // uint64_t count_bu(int n);
 // uint64_t sol(int n);;
@@ -9,9 +7,9 @@ using namespace std;
 #include "test/001.h"
 
 int main() {
-  // run_tests("BF", count_bf);
+  run_tests("BF", count_bf);
   // run_tests("TD", count_td);
   // run_tests("BU", count_bu);
-  run_tests("sol", sol);
+  // run_tests("sol", sol);
   return 0;
 }
--- a/dp/003.cpp
+++ a/dp/003.cpp
@@ -1,7 +1,5 @@
-#include <vector>
-using namespace std;
 
-// int max_subarr_bf(const vector<int>& vec);
+int max_subarr_bf(const vector<int>& vec);
 // int max_subarr_td(const vector<int>& vec);
 // int max_subarr_bu(const vector<int>& vec)
 // int sol(const vector<int>& vec);
@@ -9,9 +7,9 @@ using namespace std;
 
 #include "test/003.h"
 int main() {
-  // run_tests("bf", max_subarr_bf);
+  run_tests("bf", max_subarr_bf);
   // run_tests("td", max_subarr_td);
   // run_tests("bu", max_subarr_bu);
-  run_tests("sol", sol);
+  // run_tests("sol", sol);
   return 0;
 }

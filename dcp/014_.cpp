cf92b34486f65b09c29b58b093da04b534233ab2
NF <noah.ftnx@gmail.com>
2022-01-04 18:08:53 +0200

    m

--- dcp/014_.cpp
+++ dcp/014_.cpp
@@ -1,20 +1,21 @@
 #include <cstdlib>
 
+#include <iostream>
+using namespace std;
 double monte_carlo(int N) {
   double pi;
-
   int points=0, points_in_circle=0;
 
   for (int i=0; i<N; i++) {
-    // random point in square
+    // random point in square: values are from 0 to 1
     double x = (double) (rand()%(N+1))/N;
     double y = (double) (rand()%(N+1))/N;
     points++;
 
     double dist = x*x + y*y;
-    if (dist <=1) points_in_circle++;
+    if (dist < 1) points_in_circle++;
 
-    pi = 4.0* points_in_circle / points;
+    pi = 4.0 * points_in_circle / points;
   }
 
   return pi;

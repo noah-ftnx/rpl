bb7b9ee08da23676280953756a75a29658966d02
NF <noah.ftnx@gmail.com>
2022-01-27 16:57:04 +0200

    m

--- dcp/030_.cpp
+++ dcp/030_.cpp
@@ -61,10 +61,6 @@ int overkill(vector<int> walls) {
   return result;
 }
 
-// 3 0 2
-// 1 2 3
-// 3 2 1
-// 3 1 0 4 0 6
 
 int capacity(vector<int> walls) {
   int left = 0, right = (int) walls.size() -1;

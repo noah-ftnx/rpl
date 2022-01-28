5dff2483695b983fd470ae57ad17732a4841313c
NF <noah.ftnx@gmail.com>
2022-01-28 14:13:24 +0200

    m

--- ds/10g/09-g-bellman-ford_.cpp
+++ ds/10g/09-g-bellman-ford_.cpp
@@ -20,15 +20,20 @@ struct Graph {
     vector<int> dist(V, INT_MAX);
     dist[src]=0;
     int relaxations=V-1;
-    while (relaxations--) {
+    auto relax_edges = [&](bool check_neg_cycle=false) -> bool {
       for (auto edge: edges) {
-        if (
-            dist[edge.src] != INT_MAX && // no need to check on this case
+        if (dist[edge.src] != INT_MAX && // no need to check on this case
             dist[edge.dest] > dist[edge.src] + edge.weight) {
           dist[edge.dest] = dist[edge.src] + edge.weight;
+          if (check_neg_cycle) return true;
         }
       }
-    }
+      return false;
+    };
+    while (relaxations--) relax_edges();
+
+    if (relax_edges(true)) return {};
+
     return dist;
   }
 };
--- ds/10g/09-g-bellman-ford_path_.cpp
+++ ds/10g/09-g-bellman-ford_path_.cpp
@@ -22,16 +22,21 @@ struct Graph {
     vector<int> parent(V, -1);
     dist[src]=0;
     int relaxations=V-1;
-    while (relaxations--) {
-      for (auto edge: edges) {
-        if (
-            dist[edge.src] != INT_MAX && // no need to check on this case
-                dist[edge.dest] > dist[edge.src] + edge.weight) {
+    auto relax_edges = [&](bool check_neg_edges=false) {
+      for (auto edge : edges) {
+        if (dist[edge.src] != INT_MAX &&  // no need to check on this case
+            dist[edge.dest] > dist[edge.src] + edge.weight) {
           dist[edge.dest] = dist[edge.src] + edge.weight;
-          parent[edge.dest]=edge.src;
+          parent[edge.dest] = edge.src;
+          if (check_neg_edges) return true;
         }
       }
-    }
+      return false;
+    };
+
+    while (relaxations--) relax_edges();
+
+    if (relax_edges(true)) return {};
 
     deque<int> path;
     int u = tgt;

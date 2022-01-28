5abb363727b00d5ea4c19f573842227b982e1342
NF <noah.ftnx@gmail.com>
2022-01-28 18:01:54 +0200

    m

--- dcp/test/032.h
+++ dcp/test/032.h
@@ -25,9 +25,21 @@ vector<vector<double>> input2() {
   return rates;
 }
 
+vector<vector<double>> input3() {
+  return {
+      {1, 0.23, 0.25, 16.43, 18.21, 4.94},
+      {4.34, 1, 1.11, 71.40, 79.09, 21.44},
+      {3.93, 0.90, 1, 64.52, 71.48, 19.37},
+      {0.061, 0.014, 0.015, 1, 1.11, 0.30},
+      {0.055, 0.013, 0.014, 0.90, 1, 0.27},
+      {0.20, 0.047, 0.052, 3.33, 3.69, 1},
+  };
+}
+
 void run_tests() {
   test(input1(), true);
   test(input2(), false);
+  test(input3(), true);
 }
 
 #endif  // RPL_DCP_TEST_032_H_

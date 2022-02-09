ce9e531cbfec2f829fa47ff52acaef422135f269
NF <noah.ftnx@gmail.com>
2022-02-09 18:09:26 +0200

    m

---
+++ a/dp/tuf/dp014.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+bool subset_sumBF(vector<int> input, int k);
+
+bool subset_sumTD(vector<int> input, int k);
+
+bool subset_sumBU(vector<int> input, int k);
+
+bool subset_sumOPT(vector<int> input, int k);
+
+
+
+#include "test/dp014.h"
+int main() {
+  run_tests("BF", subset_sumBF);
+  run_tests("MMZ", subset_sumTD);
+  run_tests("BU", subset_sumBU);
+  run_tests("OPT", subset_sumOPT);
+
+  print_errors();
+  return 0;
+}

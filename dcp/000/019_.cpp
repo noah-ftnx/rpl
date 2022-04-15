989d27a5cae4e5ac5c519127c7c6b90ef35bc9b1
NF <noah.ftnx@gmail.com>
2022-04-15 11:40:12 +0300

    m

--- dcp/000/019_.cpp
+++ dcp/000/019_.cpp
@@ -1,53 +1,89 @@
+#include <climits>
 #include <vector>
 using namespace std;
 
-int paint_houses(vector<vector<int>> input){
-  if (input.empty() || input[0].empty()) return -1;
+// #define BRUTE_FORCE
 
-  const int N = input.size();
-  const int K = input[0].size();
+#ifndef BRUTE_FORCE
+struct ColCost {
+  int color; int cost;
+};
 
-  if (N>1 && K<2) return -1;  // more than 1 house, with not enough colors
-
-  // H1: 2 5 1 5
-  // H2: 1 0 3 2
-  // H3: 1 1 2 3
-  // H4: 4 2 3 1
-  struct Min {
-    int color;
-    int cost = INT_MAX;
-  };
-
-  auto set_min = [] (int cost, int color, Min& m1, Min& m2) {
-    if (cost < m1.cost) {
-      m2=m1;
-      m1.color=color;
-      m1.cost=cost;
-    } else if (cost < m2.cost) {
-      m2.color=color;
-      m2.cost=cost;
+void min2(vector<int> row, ColCost& c1, ColCost& c2) {
+  c1 = {-1, INT_MAX};
+  c2 = {-1, INT_MAX};
+  for (int i=0; i< (int) row.size(); i++) {
+    if (row[i] < c1.cost) {
+      c2=c1;
+      c1.color=i;
+      c1.cost=row[i];
+    } else if (row[i] < c2.cost) {
+      c2.color=i;
+      c2.cost=row[i];
     }
-  };
-
-  Min m1, m2;
-  for (int i=0; i<K; i++) { // first house
-    const int cost = input[0][i];
-    set_min(cost, i, m1, m2);
   }
-
-  for (int house=1; house<N; house++) { // other houses (if any)
-    Min c1, c2;
-    for (int color=0; color<K; color++) {
-      int prev_cost = color==m1.color? m2.cost : m1.cost;
-      int cost = input[house][color] + prev_cost;
-      set_min(cost, color, c1, c2);
-    }
-    m1=c1;
-    m2=c2;
-  }
-
-  return m1.cost;
 }
+
+int paint_houses(vector<vector<int>> input) {
+  if (input.empty()) return -1; // no houses
+
+  const int N = (int) input.size();
+  const int K = (int) input[0].size();
+  if (K == 0) return -1; // no colors
+  else if (N>1 && K==1) return -1;  // many houses, not enough colors
+
+  ColCost c1,c2;
+  vector<int> row = input[0];
+  min2(row, c1, c2);
+
+  for (int house=1; house<N; house++) {
+    for (int color=0; color<K; color++) {
+      row[color]= input[house][color]+
+                   (color==c1.color? c2.cost:c1.cost);
+    }
+    min2(row, c1, c2);
+  }
+
+  return c1.cost;
+}
+#endif
+
+
+//////////////////////////
+////////////// BRUTE FORCE
+//////////////////////////
+
+#ifdef BRUTE_FORCE
+int BF(const vector<vector<int>>& input,
+       int cur_cost, int house, int prev_color) {
+  const int N = (int) input.size();
+  const int K = (int) input[0].size();
+
+  if (house==N) return cur_cost;
+
+  int mn_cost = INT_MAX;
+  for (int color=0; color<K; color++) {
+    if (color==prev_color) continue;
+
+    mn_cost=min(mn_cost,
+                  BF(input, cur_cost+input[house][color], house+1, color));
+  }
+
+  return mn_cost;
+}
+
+int paint_houses(vector<vector<int>> input) {
+  if (input.empty()) return -1;
+
+  const int N = (int) input.size();
+  const int K = (int) input[0].size();
+  if (K == 0) return -1;
+  else if (N>1 && K==1) return -1;
+
+  return BF(input, 0, 0, -1);
+}
+
+#endif
 
 
 

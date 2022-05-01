435fbd6422d051b7fcd0861a6abd9a7d668cd00d
NF <noah.ftnx@gmail.com>
2022-05-01 14:56:39 +0300

    m

--- a/bt/rec-l19_.cpp
+++ a/bt/rec-l19_.cpp
@@ -1,54 +1,66 @@
+/**
+0: blocked
+
+
+
+DLRU
+
+  */
+
 #include <vector>
 #include <string>
 using namespace std;
 
-void solve(const vector<vector<int>> &grid,
-           vector<string> &res, vector<vector<bool>> &visited,
-           string cur, int i, int j) {
-  const int N = (int) grid.size();
-  const int M = (int) grid[0].size();
 
-  auto isValid = [&](int i, int j) {
-    return (i>=0 && j>=0 && i<N && j<M && grid[i][j] && !visited[i][j]);
+bool isBlocked(const vector<vector<int>> &m, int i, int j) {
+  return m[i][j]==0;
+}
+
+void solve(const vector<vector<int>> &m,
+  vector<vector<bool>> &visited,
+  vector<string> &res, string cur, int i, int j) {
+  const int N = m.size();
+  const int M = m[0].size();
+  auto isValid = [&] (int i, int j) -> bool {
+    const bool inBounds {i>=0 && j>=0 && i<N && j<M};
+    return (inBounds && !isBlocked(m, i, j) && !visited[i][j]);
   };
 
-  // base cases:
-  // blocked starting/ending points
-  if (i==0 && j==0 && (!isValid(0, 0) || !isValid(N-1, M-1))) return;
-
-  // reached the end (one of solutions)
-  if (i==N-1 && j==M-1) {
+  // base case: reached the end
+  if (i==N-1 && j == M-1) {
     res.push_back(cur);
     return;
   }
-  //
 
-  string DIR { "DLRU" };
-  vector<int> I {1,  0, 0, -1};
-  vector<int> J {0, -1, 1,  0};
+  // explore valid directions: DLRU
+  string DIR {"DLRU"};
+  vector<int> I {+1, +0, +0, -1};
+  vector<int> J {+0, -1, +1, +0};
 
-  // visit: DLRU (lexicograpically)
   for (int x=0; x<4; x++) {
-    char c = DIR[x];
-    int ii=i+I[x], jj=j+J[x];
+    int nextI = i+I[x];
+    int nextJ = j+J[x];
+    char dir = DIR[x];
 
-    if (isValid(ii, jj)) {
-      // CHECKS: works if i visited ii, jj ?
+    if (isValid(nextI, nextJ)) {
       visited[i][j]=true;
-      solve(grid, res, visited, cur+c, ii, jj);
+      solve(m, visited, res, cur+dir, nextI, nextJ);
       visited[i][j]=false;
     }
   }
+
 }
 
 vector<string> findPath(vector<vector<int>> &m) {
-  if (m.empty() || m[0].empty()) return {""};
-  const int N = (int) m.size();
-  const int M = (int) m[0].size();
-  vector<vector<bool>> visited(N, vector<bool>(M, false));
+  if (m.empty() || m[0].empty()) return {};
+
+  const int N = m.size();
+  const int M = m[0].size();
+  if (isBlocked(m, 0, 0) || isBlocked(m, N-1, M-1)) return {};
 
   vector<string> res;
-  solve(m, res, visited, "", 0, 0);
+  vector<vector<bool>> visited (N, vector<bool>(M, false));
+  solve(m, visited, res, "", 0, 0);
   return res;
 }
 

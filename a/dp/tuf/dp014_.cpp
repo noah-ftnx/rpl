a923e3396776b897089a0ea04dfe061aaab9eb36
NF <noah.ftnx@gmail.com>
2022-05-08 11:59:52 +0300

    m

--- a/dp/tuf/dp014_.cpp
+++ a/dp/tuf/dp014_.cpp
@@ -4,19 +4,18 @@ using namespace std;
 bool BF(int idx, int k, vector<int> &arr) {
   // base cases:
   if (k==0) return true; // solved
-  else if (idx==0) return arr[0]==k; // last element: solves or not
+  else if (idx==0) return arr[0]==k; // last element
 
-  // case 1: pick
-  bool pick=(k>=arr[idx]) && BF(idx-1, k-arr[idx], arr);
-
-  // case 2: not pick
+  bool pick = (k-arr[idx]>=0) && BF(idx-1, k-arr[idx], arr);
   bool notPick = BF(idx-1, k, arr);
 
-  return pick | notPick; // bitwise or: also works!
+  return pick || notPick;
 }
 
 bool subsetSumToKBF(int n, int k, vector<int> &arr) {
-  if (n==0 || k<0) return false;
+  // edge cases:
+  if (k<0 || arr.empty()) return false;
+
   return BF(n-1, k, arr);
 }
 
@@ -25,83 +24,80 @@ bool subsetSumToKBF(int n, int k, vector<int> &arr) {
 bool MMZ(int idx, int k, vector<int> &arr, vector<vector<int8_t>> &dp) {
   // base cases:
   if (k==0) return true; // solved
-  else if (idx==0) return arr[0]==k; // last element: solves or not
+  else if (idx==0) return arr[0]==k; // last element
   else if (dp[idx][k]!=-1) return dp[idx][k];
 
-  // case 1: pick
-  bool pick=(k>=arr[idx]) && MMZ(idx-1, k-arr[idx], arr, dp);
-
-  // case 2: not pick
+  bool pick = (k-arr[idx]>=0) && MMZ(idx-1, k-arr[idx], arr, dp);
   bool notPick = MMZ(idx-1, k, arr, dp);
 
   return dp[idx][k]=(pick || notPick);
 }
 
 bool subsetSumToKMMZ(int n, int k, vector<int> &arr) {
-  if (n==0 || k<0) return false;
+  // edge cases:
+  if (k<0 || arr.empty()) return false;
+
   vector<vector<int8_t>> dp(n, vector<int8_t>(k+1, -1));
   return MMZ(n-1, k, arr, dp);
 }
 
 
 
-bool subsetSumToKBU(int n, int k, vector<int> &arr) {
-  if (n==0 || k<0) return false;
-  vector<vector<bool>> dp(n, vector<bool>(k+1, false));
+bool subsetSumToKBU(int n, int target, vector<int> &arr) {
+  // edge cases:
+  if (target<0 || arr.empty()) return false;
 
-  // base cases
-  // can happen at any idx so we do a loop:
+  vector<vector<bool>> dp(n, vector<bool>(target+1, false));
+
+  // base cases:
   for (int i=0; i<n; i++) dp[i][0]=true; // solved
-  // We could do this: (simpler..)
-  // for (int j=0; j<=k; j++) dp[0][j]=(arr[0]==j); // last element
-  // or simplify just to this:
-  dp[0][arr[0]]=true; // only the matching value can be true
-  // example: it's when we have JUST ONE element, then that element must match:
-  // arr: {3}, then dp will have:
-  // - indices:    0,     1,     2,    3
-  // - values: false, false, false, true
 
-  // explore states:
+  // for (int k=0; k<=target; k++) dp[0][k]= ((arr[0])==k);
+  // simplified. last element matches
+  if (arr[0] <=target)  dp[0][arr[0]]=true;
+  // for above:
+  // index is 0, but target can be anything.
+  // But when it is true though? Only when arr[0]==k. Just in this case.
+
   for (int idx=1; idx<n; idx++) {
-    for (int j=1; j<=k; j++) {
-      // case 1: pick
-      bool pick=(j>=arr[idx]) && dp[idx-1][j-arr[idx]];
-
-      // case 2: not pick
-      bool notPick = dp[idx-1][j];
-
-      dp[idx][j]=(pick || notPick);
+    for (int k=1; k<=target; k++) {
+      bool pick = (k-arr[idx]>=0) && dp[idx-1][k-arr[idx]];
+      bool notPick = dp[idx-1][k];
+      dp[idx][k]=(pick || notPick);
     }
   }
-  return dp[n-1][k];
+
+  return dp[n-1][target];
 }
 
 
 
-bool subsetSumToKOPT(int n, int k, vector<int> &arr) {
-  if (n==0 || k<0) return false;
+bool subsetSumToKOPT(int n, int target, vector<int> &arr) {
+  // edge cases:
+  if (target<0 || arr.empty()) return false;
 
-  vector<bool> prev;
-  vector<bool> cur(k+1, false);
+  vector<bool> prev(target+1, false);
+  vector<bool> cur(target+1, false);
 
-  cur[arr[0]]=true; // base case: last element
+  // base cases:
+  // for (int k=0; k<=target; k++) cur[k]= ((arr[0])==k);
+  cur[arr[0]]=true; // simplified. last element matches
+  cur[0]=true;
+  // we never modify 0 idx in loop. we do this once over here:
+  prev[0]=true;
 
-  // explore states:
   for (int idx=1; idx<n; idx++) {
     prev=cur;
-    cur[0]=true;  // base case: solved (k became zero)
-    for (int j=1; j<=k; j++) {
-      // case 1: pick
-      bool pick=(j>=arr[idx]) && prev[j-arr[idx]];
-
-      // case 2: not pick
-      bool notPick = prev[j];
-
-      cur[j]=(pick || notPick);
+    // we are overriding each [idx][k], so no need to worry about
+    // previous values
+    for (int k=1; k<=target; k++) {
+      bool pick = (k-arr[idx]>=0) && prev[k-arr[idx]];
+      bool notPick = prev[k];
+      cur[k]=(pick || notPick);
     }
   }
 
-  return cur[k];
+  return cur[target];
 }
 
 

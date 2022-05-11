18f775b7bbd3312b04622d4f8c0902c226780ea0
NF <noah.ftnx@gmail.com>
2022-05-11 21:47:09 +0300

    m

--- a/dp/tuf/dp028.cpp
+++ a/dp/tuf/dp028.cpp
@@ -11,8 +11,7 @@ int longestPalindromeSubseqOPT(string s);
 
 #include "test/dp028.h"
 int main() {
-  run_tests("NaiveBF", longestPalindromeSubseqNaiveBF);
-  // run_tests("BF", longestPalindromeSubseqBF);
+  run_tests("BF", longestPalindromeSubseqBF);
   // run_tests("MMZ", longestPalindromeSubseqMMZ);
   // run_tests("BU", longestPalindromeSubseqBU);
   // run_tests("OPT", longestPalindromeSubseqOPT);

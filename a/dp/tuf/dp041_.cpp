4131ed5e37464a8f69bbea5a4555e9f5a81924c9
NF <noah.ftnx@gmail.com>
2022-05-14 15:05:44 +0300

    m

--- a/dp/tuf/dp041_.cpp
+++ a/dp/tuf/dp041_.cpp
@@ -6,22 +6,20 @@ int BF(int idx, int last_idx, vector<int>& nums) {
   // base case
   if (idx==0) return 0; // picked all nums
 
-  bool can_pick = (idx==N && last_idx==N) || nums[idx-1]<nums[last_idx-1];
+  bool can_pick = (last_idx==N+1) || nums[idx-1]<nums[last_idx-1];
   if (can_pick) {
     int pick=1+BF(idx-1, idx, nums);
     int skip=0+BF(idx-1, last_idx, nums);
     return max(pick, skip);
   } else { // cannot
-    int c1=0+BF(idx-1, last_idx, nums);
-    int c2=0+BF(idx-1, idx, nums);
-    return max(c1, c2);
+    return BF(idx-1, last_idx, nums);
   }
 }
 
 int lengthOfLISBF(vector<int>& nums) {
   const int N = (int) nums.size();
   if (N<=1) return N;
-  return BF(N, N, nums);
+  return BF(N, N+1, nums);
 }
 
 

7581b4af9f63281c4e23191fff15aeb56cdefcea
NF <noah.ftnx@gmail.com>
2022-05-21 19:33:16 +0300

    m

---
+++ sde/01/07.cpp
@@ -0,0 +1,14 @@
+
+void rotate(vector<vector<int>>& matrix);
+
+
+
+#include "test/07.h"
+int main() {
+  run_tests("Sol", rotate);
+  run_tests("Sol2", rotate2);
+
+  print_report();
+  return 0;
+}
+

18f4c5fb090c0f20f8400afd1e07ff1b269845ce
NF <noah.ftnx@gmail.com>
2022-06-21 11:21:37 +0300

    m

--- ds/05t/11-create-pfix.cpp
+++ ds/05t/11-create-pfix.cpp
@@ -1,53 +1,15 @@
-#include <string> // for Tree.print() // TODO INCLUDES.. hide..
-#include <stack>
-using namespace std;
 
-struct Node {
-  Node *left{};
-  Node *right{};
-  Node *parent{};
+struct Node { // TODO
   char data;
-  Node(char val) : data{} {}
 };
 
 class Tree {
-  Node* root;
-
  public:
-  Tree(const string& postfix) {
-    stack<Node*> st;
-    for (char c: postfix) {
-      switch (c) {
-        // operator
-        case '+':
-        case '-':
-        case '*':
-        case '\\':
-        case '^':
-        {
-          // create 2 nodes:
-          auto right = st.top(); st.pop();
-          auto left = st.top(); st.pop();
-          auto par = new Node(c);
+  Tree(const string& postfix) { // TODO
 
-          // linkage
-          par->left=left;
-          par->right=right;
-          right->parent=left->parent=par;
-
-          // push back
-          st.push(par);
-        }
-        break;
-
-        default: // number
-          st.push(new Node(c));
-      }
-    }
-    root = st.top(); st.pop();
   }
-  // implemented (for verification)
-  string to_string();
+
+  string to_string();  // given
 };
 
 
--- ds/05t/13-bst-del.cpp
+++ ds/05t/13-bst-del.cpp
@@ -1,15 +1,13 @@
-#include <vector> // for add
-#include <string> // for to_string
-using namespace std;
 
-struct Node;
+struct Node; // TODO
 
 struct Tree {
 
+  void delete_node(int tgt); // TODO
+
+
   void add(const vector<int>& data, const string& path); // given
   string to_string(); // given
-
-  void delete_node(int tgt);
 };
 
 

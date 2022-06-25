fb652b21809b50082727ab3035f0503b369b85e4
NF <noah.ftnx@gmail.com>
2022-06-25 11:12:49 +0300

    m

--- ds/10g/test/11.h
+++ ds/10g/test/11.h
@@ -19,13 +19,19 @@ void test(fptr function, const vector<vector<int>> matrix, int correct) {
 
 // Driver code
 int run_tests(fptr function){
-  vector<vector<int>> matrix = { { INF, 10, 15, 20 },
-                                 { 10, INF, 35, 25 },
-                                 { 15, 35, INF, 30 },
-                                 { 20, 25, 30, INF } };
-  test(function, matrix, 80);
+  vector<vector<int>> m1 = { { INF, 10, 15, 20 },
+                             { 10, INF, 35, 25 },
+                             { 15, 35, INF, 30 },
+                             { 20, 25, 30, INF } };
+  test(function, m1, 80);
+
+  vector<vector<int>> m2 = { {INF, 4, INF, 2, 1 },
+                             { 4, INF, 2, 1, INF },
+                             { INF, 2, INF, 3, 9 },
+                             { 1, INF, 9, 1, INF } };
+  test(function, m2, 10);
+
   return 0;
 }
-
 
 #endif  // _H_

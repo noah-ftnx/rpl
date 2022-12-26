cd9d3a8fc788e84164c692ed088ebaf927f15653
NF <noah.ftnx@gmail.com>
2022-12-26 16:11:04 +0200

    m

--- sde/23/09_.cpp
+++ sde/23/09_.cpp
@@ -1,31 +1,44 @@
 class Solution {
-  void dfs(int V, vector<int> adj[],
-           vector<bool> &visited, deque<int> &toposort, int i) {
-    visited[i]=true;
+    bool toposortDFS(int V, vector<int> adj[],
+                     vector<bool> &visited,
+                     vector<bool> &in_stack,
+                     deque<int> &toposort, int i) {
+        visited[i]=true;
+        in_stack[i]=true;
 
-    for (int nei: adj[i]) {
-      if (!visited[nei]) dfs(V, adj, visited, toposort, nei);
+        for (int nei: adj[i]) {
+            if (visited[nei]) {
+                if (in_stack[nei]) return true; // cycle detected (back-edge)
+            } else {
+                if (toposortDFS(V, adj, visited, in_stack, toposort, nei)) return true; // cycle
+            }
+        }
+
+        in_stack[i]=false;
+        toposort.push_front(i);
+
+        return false;
     }
 
-    toposort.push_front(i);
-  }
+public:
+    //Function to return list containing vertices in Topological order.
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        deque<int> toposort;
+        vector<bool> visited(V, false);
 
- public:
-  //Function to return list containing vertices in Topological order.
-  vector<int> topoSort(int V, vector<int> adj[])
-  {
-    deque<int> toposort;
-    vector<bool> visited(V, false);
+        for (int i=0; i<V; i++) {
+            vector<bool> in_stack(V, false);
+            if (!visited[i]) {
+                if(toposortDFS(V, adj, visited, in_stack, toposort, i)) return {}; // cycles
+            }
+        }
 
-    for (int i=0; i<V; i++) {
-      if (!visited[i]) dfs(V, adj, visited, toposort, i);
+        vector<int> res(toposort.begin(), toposort.end());
+
+        // could have push_back to a vector (in DFS) and reverse it here:
+        // reverse(toposort.begin(), toposort.end());
+
+        return res;
     }
-
-    vector<int> res;
-    res.insert(res.begin(), toposort.begin(), toposort.end());
-    // could have push_back to a vector (in DFS) and reverse it here:
-    // reverse(toposort.begin(), toposort.end());
-
-    return res;
-  }
 };

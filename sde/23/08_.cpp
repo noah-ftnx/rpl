519a6060c98afeedd98f471db3196540db6970ee
NF <noah.ftnx@gmail.com>
2022-12-26 15:32:42 +0200

    m

--- sde/23/08_.cpp
+++ sde/23/08_.cpp
@@ -1,29 +1,42 @@
-class Solution {
- public:
-  //Function to return list containing vertices in Topological order.
-  vector<int> topoSort(int V, vector<int> adj[])
-  {
-    // find indegrees
-    vector<int> indegree(V, 0);
-    for (int i=0; i<V; i++)
-      for (int nei: adj[i]) indegree[nei]++;
-	        
-    // graph entrypoints
-    queue<int> q;
-    for (int i=0; i<V; i++) if (indegree[i]==0) q.push(i);
-	   
-    vector<int> toposort;
-    while(!q.empty()) {
-      int vertex = q.front(); q.pop();
-      toposort.push_back(vertex);
-	       
-      for (int nei: adj[vertex]) {
-        if(--indegree[nei]==0) {
-          q.push(nei);
+class Solution
+{
+public:
+    //Function to return list containing vertices in Topological order.
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        // Khans algo
+        // BFS: add edges w/ zero indegree to a worklist
+        // when visiting: decrease indegrees
+        vector<int> indegree(V, 0);
+        for (int from=0; from<V; from++) {
+            for (int to: adj[from]) { // neighbor
+                indegree[to]++;
+            }
         }
-      }
+
+        queue<int> worklist;
+        for (int i=0; i<V; i++) {
+            if (indegree[i]==0) worklist.push(i);
+        }
+
+        vector<int> order; // will be IN CORRECT ORDER!
+
+        // BFS
+        while (!worklist.empty()) {
+            int node = worklist.front(); worklist.pop();
+            order.push_back(node);
+
+            for (int nei: adj[node]) {
+                // decrease indegree (satisfied that contraint)
+                if (--indegree[nei]==0) {
+                    worklist.push(nei);
+                }
+            }
+        }
+
+        // topo order size doesn't match? -> cycle
+        if(order.size() != V) return {}; // empty ordering
+
+        return order; // no it's not rev anymore!
     }
-	   
-    return toposort.size() == V? toposort : vector<int>();
-  }
 };

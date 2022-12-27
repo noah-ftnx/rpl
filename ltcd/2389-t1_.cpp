897ff70d96aacd4f407e7d0a5343fa9f926273f7
NF <noah.ftnx@gmail.com>
2022-12-27 14:27:41 +0200

    m

--- ltcd/2389-t1_.cpp
+++ ltcd/2389-t1_.cpp
@@ -1,42 +1,3 @@
-/*
- *
-IDEA:
-nums: n
-queries: m
-
-vector<answer>: size m
-- ans[i]: max size of subsequence from nums -> sum <=queries[i]
-
-nums: 4, 5, 2, 1
-quer: 4, 10, 21
-
-nums: 4,5
-query: 10
-
-1. sort nums & take sum
-sum: 12
-1, 2, 3, 4 (sort asc)
-
-2. for each q: queries (slower: N*M)
-int cnt, sum
-for each num:
-    update sum, cnt
-    as long as <= q
-append result
-
-
-2.opt sort queries, ascending N+M CAUSES ISSUES ... on computing result
-
-int cnt, sum = 0
-num_idx
-for each q: queries:
-  for each num: (remeber num_idx)
-    update cnt, sum
-    as long as <= q
-append result
-*/
-
-
 #include <algorithm>
 using namespace std;
 
@@ -61,7 +22,6 @@ public:
                 int num = nums[num_idx];
                 // exceeded limits
                 if (sum+num > query) break;
-
                 sum+=num;
                 cnt++;
             }
@@ -69,6 +29,7 @@ public:
             result_mp.insert(make_pair(query, cnt));
         }
 
+        // build result in correct order
         vector<int> result;
         for (int query: queries) {
             result.push_back(result_mp[query]);

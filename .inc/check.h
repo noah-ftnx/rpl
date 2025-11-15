0c53181a4978837dfacd55089019da3ca340e2a2
NF <noah.ftnx@gmail.com>
2025-11-15 12:52:02 +0000

    backup: 25-11-15 12:52:02

--- .inc/check.h
+++ .inc/check.h
@@ -8,8 +8,6 @@
 
 #include "to_string.h"
 
-using namespace std;
-
 void check_bool(string msg, bool result, bool correct) {
   cout << msg << ": " << boolalpha << result << (result != correct? " (WRONG)":"") << endl;
 }
--- ds/05t/test/bst-09.h
+++ ds/05t/test/bst-09.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <assert.h>
+using namespace std;
 #include "../../../.inc/check.h"
 
 

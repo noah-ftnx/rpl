20efd8a7246b7dd89fd6b9bd99c7501c25a2a8a9
NF <noah.ftnx@gmail.com>
2025-11-15 20:14:55 +0000

    backup: 25-11-15 20:14:55

--- ds/05t/12-qsucc.cpp
+++ ds/05t/12-qsucc.cpp
@@ -1,6 +1,14 @@
+#include <vector>
+#include <deque>
+using namespace std;
 
-// TEMPLATE
+// TODO: define Node
+// left, right, parent, data (templated)
+
+// TODO: Tree TEMPLATE
 struct Tree {
+  Node<T>* root {};
+  Tree(T value) { root = new Node<T>(value); }
 
   Tree(T value);
   void add(const vector<T>& data, const string& path); // given
@@ -10,6 +18,6 @@ struct Tree {
 };
 
 
-
+#include <string>
 #include "test/12.h"
 int main() { run_tests(); return 0; }

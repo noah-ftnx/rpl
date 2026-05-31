7b76c92dd17393e4d8c1d6e4908f6aeb37a8e8a6
NF <noah.ftnx@gmail.com>
2026-05-31 19:11:27 +0100

    backup: 26-05-31 19:11:27

--- sde/03/test/06.h
+++ sde/03/test/06.h
@@ -23,6 +23,16 @@ void run_tests(fptr fun, string msg) {
   test(fun, {1,3,2,3,1}, 2);
   test(fun, {2,4,3,5,1}, 3);
   test(fun, {}, 0);
+  test(fun, {1,1,1,1}, 0);
+  test(fun, {5,4,3,2,1}, 4);
+  test(fun, {9,1,2,3,1}, 5);
+  test(fun, {10,5,2,1}, 4);
+  test(fun, {1,2,3,4,5,6}, 0);
+  test(fun, {4,1,2,8,3,0}, 7);
+  test(fun, {2147483647, 1073741824, 1, 0}, 5);
+  test(fun, {-1,-2,-3,-4}, 6);
+  test(fun, {-5,-5,-5}, 3);
+  test(fun, {0,0,0,0,0}, 0);
   cout << endl;
 }
 
